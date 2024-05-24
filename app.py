b3469ad31ef338e810e1d687c77985f3460b4f32
Kanchan Rai <kanchanrai2307@gmail.com>
2024-05-24 16:27:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -311,15 +311,15 @@ elif selected == "Breast Cancer Prediction":
     st.title("Breast Cancer Prediction using ML")
 
     breast_cancer_features = [
-    'mean radius', 'mean texture', 'mean perimeter', 'mean area', 'mean smoothness',
-    'mean compactness', 'mean concavity', 'mean concave points', 'mean symmetry',
-    'mean fractal dimension', 'radius error', 'texture error', 'perimeter error',
-    'area error', 'smoothness error', 'compactness error', 'concavity error',
-    'concave points error', 'symmetry error', 'fractal dimension error',
-    'worst radius', 'worst texture', 'worst perimeter', 'worst area',
-    'worst smoothness', 'worst compactness', 'worst concavity',
-    'worst concave points', 'worst symmetry', 'worst fractal dimension'
-]
+        'mean radius', 'mean texture', 'mean perimeter', 'mean area', 'mean smoothness',
+        'mean compactness', 'mean concavity', 'mean concave points', 'mean symmetry',
+        'mean fractal dimension', 'radius error', 'texture error', 'perimeter error',
+        'area error', 'smoothness error', 'compactness error', 'concavity error',
+        'concave points error', 'symmetry error', 'fractal dimension error',
+        'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+        'worst smoothness', 'worst compactness', 'worst concavity',
+        'worst concave points', 'worst symmetry', 'worst fractal dimension'
+    ]
 
 # Organize input fields in columns
 col1, col2, col3, col4, col5 = st.columns(5)
